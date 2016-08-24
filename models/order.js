0f83f146fba34aa7c3e40495e488c3a075c30cc8
Hussain <mukadam59@gmail.com>
2016-08-24 19:55:33 +0530

    Added database files and upsert page.

---
+++ models/order.js
@@ -0,0 +1,11 @@
+"use strict"
+module.exports = function(sequelize, DataTypes){
+    var Order = sequelize.define('Order', {
+        customerName: {type: DataTypes.STRING, allowNull: false},
+        productCode: {type: DataTypes.STRING, allowNull: false},
+        unitsOrdered: {type: DataTypes.INTEGER, allowNull: false},
+        unitPrice: {type: DataTypes.INTEGER, allowNull: false},
+        status: {type: DataTypes.STRING, allowNull: false, defaultValue: 'PENDING'}
+    });
+    return Order;
+};
--- routes.js
+++ routes.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Customer Order Reporting Tool' });
 });
 
+router.get('/create', function (req, res, next) {
+    res.render('upsert');
+});
+
 
 
 module.exports = router;
